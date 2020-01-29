f369a348663b7ab7bddc0c6500fad8203e73d65b
Bashir Egeh <b_egeh@hotmail.com>
2020-01-29 19:40:32 +0000

    added ajax post and get requests to client.js in order collect data from new tweet input

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,41 +4,43 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const convertDate = function(date){
+const convertDate = (date) => {
   let timeSince = (new Date()).getTime() - date;
-  
-  if(timeSince % 31556952000 > 0){
-    if(timeSince % 31556952000 > 0 === 1){
-      return Math.floor(timeSince / 31556952000) + ' year ago';
-    } else{
-      return Math.floor(timeSince / 31556952000) + ' years ago';
+  console.log(timeSince);
+
+  if (Math.floor(timeSince / 31556952000) > 0) {
+    if (Math.floor(timeSince / 31556952000) === 1) {
+      return Math.floor(timeSince / 31556952000) + ' year';
+    } else {
+      return Math.floor(timeSince / 31556952000) + ' years';
     }
-  } else if(timeSince % 2592000000	 > 0){
-    if(timeSince % 2592000000	 > 0 === 1){
-      return Math.floor(timeSince / 2592000000) + ' a month ago';
-    } else{
-      return Math.floor(timeSince / 2592000000) + ' months ago';
+  } else if (Math.floor(timeSince / 2592000000) > 0) {
+    if (Math.floor(timeSince / 2592000000) === 1) {
+      return Math.floor(timeSince / 2592000000) + ' a month';
+    } else {
+      return Math.floor(timeSince / 2592000000) + ' months';
     }
-  } else if(timeSince % 86400000 > 0){
-    if(timeSince % 86400000 > 0 === 1){
-      return Math.floor(timeSince / 86400000) + ' day ago';
-    } else{
-      return Math.floor(timeSince / 86400000) + ' days ago';
+  } else if (Math.floor(timeSince / 86400000) > 0) {
+    if (Math.floor(timeSince / 86400000) === 1) {
+      return Math.floor(timeSince / 86400000) + ' day';
+    } else {
+      return Math.floor(timeSince / 86400000) + ' days';
     }
-  } else if(timeSince % 3600000 > 0){
-    if(timeSince % 3600000 > 0 === 1){
-      return Math.floor(timeSince / 3600000) + ' an hour ago';
-    } else{
-      return Math.floor(timeSince / 3600000) + ' hours ago';
+  } else if (Math.floor(timeSince / 3600000) > 0) {
+    if (Math.floor(timeSince / 3600000) === 1) {
+      return Math.floor(timeSince / 3600000) + ' an hour';
+    } else {
+      return Math.floor(timeSince / 3600000) + ' hours';
     }
-  } else{
-    if(timeSince % 1000 > 0 === 1){
-      return Math.floor(timeSince / 1000) + ' a second ago';
-    } else{
-      return Math.floor(timeSince / 1000) + ' seconds ago';
+  } else if (Math.floor(timeSince / 1000) === 1) {
+    if (Math.floor(timeSince / 1000) === 1) {
+      return Math.floor(timeSince / 1000) + ' a second';
+    } else {
+      return Math.floor(timeSince / 1000) + ' seconds';
     }
+  } else {
+    return `0 seconds`;
   }
-  
 }
 
 const renderTweets = function (tweets) {
@@ -60,7 +62,7 @@ const createTweetElement = function (tweet) {
   <p class='the-tweet'>${tweet.content.text}</p>
 </div>
 <footer>
-  <p>${convertDate(tweet.created_at)}</p>
+  <p>${moment(tweet.created_at).fromNow()}</p>
   <div class='icons-container'>
     <img class='icons' src="https://img.icons8.com/material-sharp/24/000000/flag.png">
     <img class='icons' src="https://img.icons8.com/material-sharp/24/000000/retweet.png">
@@ -70,39 +72,27 @@ const createTweetElement = function (tweet) {
 </article>`
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
-$(document).ready(function(){
-  renderTweets(data);
+$(document).ready(function () {
+
+  $(function () {
+    const $submit = $('#text-form');
+    $submit.on('submit', function (event) {
+      event.preventDefault();
+      console.log('Button clicked, performing ajax call...');
+      console.log($submit.serialize().length);
+      if ($submit.serialize() === `text=` || $submit.serialize() === null) {
+        alert('Field is empty. Please enter tweet before submitting.');
+      } else if ($submit.serialize().length > 145) {
+        alert("You are over 140 character limit. Please reduce length of tweet and resubmit");
+      } else {
+        $.ajax('/tweets', { method: 'POST', data: $submit.serialize() })
+      }
+    });
+  });
+
+  const loadTweets = $.ajax('/tweets', { method: 'GET', datatype: 'json', success: function (res) { renderTweets(res); } });
+
+  console.log(loadTweets);
+
 });
-
-
-// const $tweet = createTweetElement(data);
-
-// Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
